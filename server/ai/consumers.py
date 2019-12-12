1aaa06ac070164411da8b2db22acf358212635e8
Nikita Korobkov <nikkorobk@gmail.com>
2019-12-12 17:40:39 -0500

    Two more engines added.

--- server/ai/consumers.py
+++ server/ai/consumers.py
@@ -3,9 +3,14 @@ import os
 import torch
 from channels.generic.websocket import WebsocketConsumer
 import json
+
+from minimax_policy.MiniMaxPolicy import MiniMaxPolicy
+from minimax_policy.ModelGuidedMiniMax import ModelGuidedMiniMax
+from policy.ModelTreeD2Policy import ModelTreeD2Policy
 from server.main.settings import BASE_DIR
 
 
+from minimax_policy.evaluator.SimpleEvaluators import ActiveCountEvaluator
 from game.GameState import GameState
 from policy.ModelBasedPolicy import ModelBasedPolicy
 from rl.feature.PlainFearutesExtractor import PlainFeatureExtractor
@@ -24,22 +29,41 @@ class AIConsumer(WebsocketConsumer):
         size_w = text_data_json['sizeW']
         to_move = text_data_json['toMove']
         game_state = GameState.from_field_list(h=size_h, w=size_w, field=field, to_move=to_move)
-
+        print('state received, finding a move')
         self.send(text_data=json.dumps({
             'move': self.engine.get_move(game_state)
         }))
-
-        self.send(text_data=json.dumps({
-            'move': self.engine.get_move(game_state)
-        }))
+        print('move sent')
 
 class TonyAIConsumer(AIConsumer):
     def connect(self):
         self.h, self.w = 8, 8
         model = ConvolutionValue(self.h, self.w)
-        print(BASE_DIR)
         model.load_state_dict(torch.load(os.path.join(BASE_DIR, 'ai/trained_models/conv8value.pt')))
         feature_extractor = PlainFeatureExtractor()
         self.engine = ModelBasedPolicy(model, feature_extractor, self.h, self.w)
         self.accept()
 
+
+class JessieAIConsumer(AIConsumer):
+    def connect(self):
+        self.h, self.w = 8, 8
+        model = ConvolutionValue(self.h, self.w)
+        model.load_state_dict(torch.load(os.path.join(BASE_DIR, 'ai/trained_models/conv8value.pt')))
+        feature_extractor = PlainFeatureExtractor()
+        evaluatorActiveCells = ActiveCountEvaluator()
+
+        self.engine = ModelGuidedMiniMax(model,
+                                         feature_extractor,
+                                         self.h,
+                                         self.w,
+                                         evaluatorActiveCells,
+                                         lambda x: 30,
+                                         depth=3)
+        self.accept()
+
+class MaxAIConsumer(AIConsumer):
+    def connect(self):
+        evaluator = ActiveCountEvaluator()
+        self.engine = MiniMaxPolicy(evaluator, 2)
+        self.accept()
--- server/ai/routing.py
+++ server/ai/routing.py
@@ -4,4 +4,6 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/ai/tony/$', consumers.TonyAIConsumer),
+    re_path(r'ws/ai/jessie/$', consumers.JessieAIConsumer),
+    re_path(r'ws/ai/max/$', consumers.MaxAIConsumer),
 ]

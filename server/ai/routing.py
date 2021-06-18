4fed1252ee24c2fa66f4649765f23c56a631b004
Nikita Korobkov <nikkorobk@gmail.com>
2021-06-18 14:20:08 -0400

    remove django stuff

--- server/ai/routing.py
+++
@@ -1,9 +0,0 @@
-from django.urls import re_path
-
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/ai/tony/$', consumers.TonyAIConsumer),
-    re_path(r'ws/ai/jessie/$', consumers.JessieAIConsumer),
-    re_path(r'ws/ai/max/$', consumers.MaxAIConsumer),
-]
